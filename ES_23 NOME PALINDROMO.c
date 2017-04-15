50a7f1717534ef177eddee8779b1d2dda8fab6d8
lollo00 <gezzi.g@libero.it>
2017-04-15 16:04:18 +0200

    Add files via upload

---
+++ ES_23 NOME PALINDROMO.c
@@ -0,0 +1,35 @@
+/* Studente: Lorenzo Gezzi 
+   Classe:3INA 
+   Data:15/04/2017
+   Versione: 1.0
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main(){
+            int cont;
+            int i;
+            char parola[100];
+            int medio;
+            printf("Inserire la parola ");
+            scanf("%s",parola);
+            cont=0;
+            while(parola[cont]!='\0'){
+                cont++;
+            }
+            cont--;
+            i=0;
+            while(parola[cont]==parola[i] && cont>i){
+                i++;
+                cont--;
+            }
+            if(cont>i){
+                printf("La parola non e' palindroma");
+            }
+            else{
+                printf("La parola e' palindroma");
+            }
+            printf("\n");
+            system("PAUSE");
+}
+            

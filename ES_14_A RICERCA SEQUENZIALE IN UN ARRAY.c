14104adfb6f80a2c28e7609738ca4c84f7ddfe79
lollo00 <gezzi.g@libero.it>
2017-04-25 14:59:03 +0200

    Add files via upload

---
+++ ES_14_A RICERCA SEQUENZIALE IN UN ARRAY.c
@@ -0,0 +1,35 @@
+/* Studente: Lorenzo Gezzi 
+   Classe:3INA 
+   Data:25/04/2017
+   Versione: 1.0
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main(){
+    int i;
+    int v[10]={10,1,5,4,3,6,2,8,7,9};
+    int n;
+    int trovato;
+    int pos;
+    printf("Inserire il numero da cercare: ");
+    scanf("%d",&n);
+    i=0;
+    trovato=0;
+    while(i<10 && !trovato){
+        if(n==v[i]){
+            pos=i;
+            trovato=1;
+        }
+        i++;
+    }
+    if(trovato==1){
+        printf("numero trovato in posizione: %d",pos);
+    }
+    else{
+        printf("numero non trovato");
+    }
+    printf("\n");
+    system("PAUSE");
+}
+
---
+++ ES_14_B RICERCA SEQUENZIALE IN UN ARRAY CON SENTINELLA.c
@@ -0,0 +1,31 @@
+/* Studente: Lorenzo Gezzi 
+   Classe:3INA 
+   Data:25/04/2017
+   Versione: 1.0
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main(){
+    int i;
+    int v[10]={10,1,5,4,3,6,2,8,7,9};
+    int n;
+    int pos;
+    printf("Inserire il numero da cercare: ");
+    scanf("%d",&n);
+    v[10]=n;
+    i=0;
+    while(v[i]!=n){
+        i++;
+    }
+    pos=i;
+    if(pos<10){
+        printf("numero trovato in posizione: %d",pos);
+    }
+    else{
+        printf("numero non trovato");
+    }
+    printf("\n");
+    system("PAUSE");
+}
+
